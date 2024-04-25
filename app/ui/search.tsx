cf1d1ef83d4f12163cf61d77fff5c9e9af624781
Andrew Martin <152824513+andrewmartinn@users.noreply.github.com>
2024-04-25 09:14:53 +0400

    updated invoices component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,6 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((searchTerm: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
 
     if (searchTerm) {
       params.set('query', searchTerm);
